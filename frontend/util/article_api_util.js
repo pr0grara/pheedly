7ffdbf6da10e5233bdf285e2145623068cfd47e8
pr0grara <azbaghda@gmail.com>
2020-06-25 19:51:27 -0700

    changing api url back to http

--- frontend/util/article_api_util.js
+++ frontend/util/article_api_util.js
@@ -2,7 +2,7 @@
 export const articles = () => {
   return $.ajax({
     method: 'GET',
-    url: 'https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=86847d711eb84d41accab719e9920a5d',
+    url: 'http://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=86847d711eb84d41accab719e9920a5d',
     data: {}
   })
 }

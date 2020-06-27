37bb4c5d29486ea72e8bfed1dd30d5418975fea2
pr0grara <azbaghda@gmail.com>
2020-06-26 17:18:24 -0700

    styling login/signup errors

--- frontend/components/session_form/session_form.jsx
+++ frontend/components/session_form/session_form.jsx
@@ -33,7 +33,7 @@ class SessionForm extends React.Component {
 
   renderErrors() {
     return (
-      <ul>
+      <ul className='errors'>
         {this.props.errors.map((error, i) => (
           <li key={`error-${i}`}>
             {error}

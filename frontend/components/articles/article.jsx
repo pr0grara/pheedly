27e3be6397d0a263cf6c9cf0c7a24d8935fd7726
pr0grara <azbaghda@gmail.com>
2020-06-26 17:06:03 -0700

    for heroku only

--- frontend/components/articles/article.jsx
+++ frontend/components/articles/article.jsx
@@ -22,7 +22,13 @@ class Article extends React.Component {
         {Object.values(this.props.articles).length === 0 ? 
           null 
           : 
-          <a href={articles[randNum].url}>{articles[randNum].title}</a> 
+          //<a href={articles[randNum].url}>{articles[randNum].title}</a> 
+          <p>this is a random article....ok? Ok cool thanks for playing along,
+             actually because you asked pulling in real articles from the web is
+             totally within my capabilities, it's just that after 100 hits to my
+             endpoint they cut me off unless I give them legal tender, which I would 
+             rather not do yet. Anyway thanks for staying updated with the current news.
+          </p>
         }
       </>
     )
